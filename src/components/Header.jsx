e5e4d94c123ced383b9671fc3fab49a688076bd5
eldadstar <eldadfikre456@gmail.com>
2024-11-23 18:11:37 +0300

    Header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import logo from '../assets/react.svg';
+import logo from '../assets/cinema.jpg';
 import '../components/header/Header.scss';
 
 const HEADER_LIST = [
@@ -50,7 +50,7 @@ const Header = () => {
         <div className="header-bar"></div>
         <div className="header-navbar">
           <div className="header-image">
-            <img src={logo} alt="" />
+            <img src={logo} className='' alt="" />
           </div>
           <div
             className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`}
