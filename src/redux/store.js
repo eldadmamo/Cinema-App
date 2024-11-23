6ddcc312dfbc15d8cbe9e0637ef709ac2c42a37f
eldadstar <eldadfikre456@gmail.com>
2024-11-23 12:44:59 +0300

    updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension';
-import { thunk } from "redux-thunk";
+import thunk  from "redux-thunk";
 import rootReducers from "./reducers";
 
 const initialState = {};
@@ -9,8 +9,8 @@ const middleware = [thunk];
 export const store = createStore(
     rootReducers,
     initialState,
-    composeWithDevTools(applyMiddleware(...middleware)
-)
+    composeWithDevTools(applyMiddleware(...middleware))
+    
 )
 
 export default store;
