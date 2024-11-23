bf0c606218c47c12b2f94244329493ee6a038603
eldadstar <eldadfikre456@gmail.com>
2024-11-23 17:47:15 +0300

    checked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import logo from '../assets/react.svg'
+import { useState } from 'react';
+import logo from '../assets/react.svg';
 import '../components/header/Header.scss';
 
 const HEADER_LIST = [
@@ -7,82 +7,74 @@ const HEADER_LIST = [
     id: 1,
     iconClass: 'fas fa-film',
     name: 'Now Playing',
-    type:'now_playing'
+    type: 'now_playing'
   },
   {
     id: 2,
     iconClass: 'fas fa-fire',
     name: 'Popular',
-    type:'popular'
+    type: 'popular'
   },
   {
     id: 3,
     iconClass: 'fas fa-film',
     name: 'Top Rated',
-    type:'top_rated'
+    type: 'top_rated'
   },
   {
     id: 4,
     iconClass: 'fas fa-plus-square',
     name: 'Upcoming',
-    type:'upcoming'
-  },
-]
+    type: 'upcoming'
+  }
+];
 const Header = () => {
   let [navClass, setNavClass] = useState(false);
   let [menuClass, setMenuClass] = useState(false);
 
   const toggleMenu = () => {
-    menuClass = !menuClass
+    menuClass = !menuClass;
     navClass = !navClass;
     setNavClass(navClass);
-    setMenuClass(menuClass)
-    if(navClass){
+    setMenuClass(menuClass);
+    if (navClass) {
       document.body.classList.add('header-nav-open');
     } else {
-      document.body.classList.remove('header-nav-open')
+      document.body.classList.remove('header-nav-open');
     }
-  }
+  };
 
   return (
     <>
-    <div className="header-nav-wrapper">
-      <div className="header-bar"></div>
-      <div className="header-navbar">
-        <div className="header-image">
-          <img src={logo} alt="" />
-        </div>
-        <div 
-           className={`${menuClass ? 'header-menu-toggle is-active': 'header-menu-toggle' }`}
-           id='header-mobile-menu'
-           onClick={() => toggleMenu()}
-           >
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-        <ul className={`${navClass ? 'header-nav header-mobile-nav': 'header-nav'}`} >
-          {
-            HEADER_LIST.map((data) => 
-              <li 
-                 key={data.id}
-                 className='header-nav-item'>
-                  <span className='header-list-name'>
+      <div className="header-nav-wrapper">
+        <div className="header-bar"></div>
+        <div className="header-navbar">
+          <div className="header-image">
+            <img src={logo} alt="" />
+          </div>
+          <div
+            className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`}
+            id="header-mobile-menu"
+            onClick={() => toggleMenu()}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
+            {HEADER_LIST.map((data) => (
+              <li key={data.id} className="header-nav-item">
+                <span className="header-list-name">
                   <i className={data.iconClass}></i>
-                  </span>
-                  &nbsp;
-                  <span className="header-list-name">{data.name}</span>
-                 </li>
-            )
-          }
-          <input 
-             className='search-input'
-             type="text"
-             placeholder='Search for a movie'
-          />
-        </ul>
+                </span>
+                &nbsp;
+                <span className="header-list-name">{data.name}</span>
+              </li>
+            ))}
+            <input className="search-input" type="text" placeholder="Search for a movie" />
+          </ul>
+        </div>
       </div>
-    </div>
     </>
   );
 };
