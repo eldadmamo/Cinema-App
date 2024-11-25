a55b143bb764b235242df0037fd90789bc304682
eldadstar <eldadfikre456@gmail.com>
2024-11-25 20:41:02 +0300

    'feature/create-grid-component'

--- src/components/content/rating/Rating.jsx
+++ src/components/content/rating/Rating.jsx
@@ -1,54 +1,50 @@
-import '../rating/Rating.scss'
+import '../rating/Rating.scss';
 import PropTypes from 'prop-types';
+import { Fragment, useEffect, useRef, useState } from 'react';
 
-import { Fragment, useEffect, useRef, useState} from 'react';
 const Rating = (props) => {
-    const {rating, totalStars, className } = props;
-    const [numberOfStars, setNumberOfStars] = useState();
-    const ratingRef = useRef();
+  const { rating, totalStars, className } = props;
+  const [numberOfStars, setNumberOfStars] = useState();
+  const ratingRef = useRef();
 
-    [1,2,3,4,5];
-
-    useEffect(() => {
-        setNumberOfStars([...Array(totalStars).keys()].map((i) => i + 1));
-        let percentage;
-        if (rating <= 5){
-            percentage = (rating / 5) * 100;
-        } else {
-            percentage = (rating / 10) * 100;
-        }
-        const startPercentage = `${Math.floor(percentage)}%`;
-        ratingRef.current.style.width = startPercentage;
-    },[rating,totalStars])
+  useEffect(() => {
+    setNumberOfStars([...Array(totalStars).keys()].map((i) => i + 1));
+    let percentage;
+    if (rating <= 5) {
+      percentage = (rating / 5) * 100;
+    } else {
+      percentage = (rating / 10) * 100;
+    }
+    const startPercentage = `${Math.floor(percentage)}%`;
+    ratingRef.current.style.width = startPercentage;
+  }, [rating, totalStars]);
 
   return (
     <div className="star-rating">
       <div className={`back-stars ${className}`}>
-        {
-            numberOfStars && numberOfStars.map((i) => (
-                <Fragment key={i}>
-                    <i className="fas fa-star" aria-hidden="true"></i>
-                </Fragment>
-            )) 
-        }
+        {numberOfStars &&
+          numberOfStars.map((i) => (
+            <Fragment key={i}>
+              <i className="fas fa-star" aria-hidden="true"></i>
+            </Fragment>
+          ))}
         <div className="front-stars" ref={ratingRef}>
-        {
-            numberOfStars && numberOfStars.map((i) => (
-                <Fragment key={i}>
-                    <i className="fas fa-star" aria-hidden="true"></i>
-                </Fragment>
-            ))
-        }
+          {numberOfStars &&
+            numberOfStars.map((i) => (
+              <Fragment key={i}>
+                <i className="fas fa-star" aria-hidden="true"></i>
+              </Fragment>
+            ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Rating.PropTypes = {
-    rating: PropTypes.number.isRequired,
-    totalStars: PropTypes.number.isRequired,
-    className: PropTypes.string
-}
+  rating: PropTypes.number.isRequired,
+  totalStars: PropTypes.number.isRequired,
+  className: PropTypes.string
+};
 
-export default Rating
+export default Rating;
